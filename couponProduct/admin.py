0600a4f8dab7da1b9f9f54073530c6d7b84de302
CEREN DIKMEN <e2043545@ceng.metu.edu.tr>
2017-02-27 00:03:41 +0200

    'Valid_from cannot be later than valid_until' warning was added to coupon add page.

--- couponProduct/admin.py
+++ couponProduct/admin.py
@@ -1,12 +1,27 @@
 from django.contrib import admin
+from django import forms
 from .models import *
 
 # Models are registered and the admin page is customized.
+
+class CouponForm(forms.ModelForm):
+    class Meta:
+        model = Coupon
+        fields = '__all__'
+
+    def clean(self):
+    	start_date = self.cleaned_data.get('valid_from')
+    	end_date = self.cleaned_data.get('valid_until')
+    	if start_date > end_date:
+    		raise forms.ValidationError("Valid_from cannot be later than valid_until.")
+    	return self.cleaned_data
+
 class CouponAdmin(admin.ModelAdmin):
-    list_display = ('code', 'coupon_type', 'amount', 'description',)
-    search_fields = ('code', 'description',)
-    list_filter = ('coupon_type','amount', 'valid_until',)
-    ordering = ('-amount',)
+	form = CouponForm
+	list_display = ('code', 'coupon_type', 'amount', 'description',)
+	search_fields = ('code', 'description',)
+	list_filter = ('coupon_type','amount', 'valid_until',)
+	ordering = ('-amount',)
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('identifier', 'price', 'description',)
