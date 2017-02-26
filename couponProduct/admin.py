9b4d18e9415d29eb3a0177484d291775ec21b80d
CEREN DIKMEN <e2043545@ceng.metu.edu.tr>
2017-02-26 23:13:01 +0200

    Admin page was customized.

--- couponProduct/admin.py
+++ couponProduct/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
 from .models import *
 
-# Register your models here.
-admin.site.register(Coupon)
-admin.site.register(Product)
-admin.site.register(Purchase)
+# Models are registered and the admin page is customized.
+class CouponAdmin(admin.ModelAdmin):
+    list_display = ('code', 'coupon_type', 'amount', 'description',)
+    search_fields = ('code', 'description',)
+    list_filter = ('coupon_type','amount', 'valid_until',)
+    ordering = ('-amount',)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('identifier', 'price', 'description',)
+    search_fields = ('identifier', 'description',)
+    list_filter = ('description', 'price',)
+    ordering = ('price',)
+    filter_horizontal = ('coupons',)
+
+class PurchaseAdmin(admin.ModelAdmin):
+    list_display = ('couponCode', 'productId', 'email')
+
+admin.site.register(Coupon, CouponAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Purchase, PurchaseAdmin)
 
